dc582f5a28ee07bcb1aeb03cd93aa42f23a0a94b
Alonso-Higa <alonsohk25@gmail.com>
2026-05-13 12:30:00 -0500

    feat(attendance): add parent attendance list

---
+++ src/attendance/presentation/views/parent-attendance-list/parent-attendance-list.component.ts
@@ -0,0 +1,52 @@
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { AttendanceStore } from '../../../application/state/attendance.store';
+import { Attendance } from '../../../domain/entities/attendance.entity';
+import {environment} from "../../../../environments/environment";
+
+
+@Component({
+    selector: 'app-parent-attendance-list',
+    standalone: true,
+    imports: [CommonModule, MatTableModule, MatIconModule, MatProgressSpinnerModule, TranslateModule],
+    templateUrl: './parent-attendance-list.component.html',
+    styleUrl: './parent-attendance-list.component.css'
+})
+export class ParentAttendanceListComponent implements OnInit {
+    readonly store = inject(AttendanceStore);
+    private readonly http = inject(HttpClient);
+
+    readonly currentParentName = signal<string>('');
+
+    displayedColumns: string[] = ['kid', 'transport', 'schedules', 'status'];
+
+    dataSource = computed(() => {
+        const parentName = this.currentParentName();
+        const allRecords = this.store.attendance();
+
+        if (!parentName) return new MatTableDataSource<Attendance>([]);
+
+        const myKids = allRecords.filter(a => a.tutorName === parentName);
+        return new MatTableDataSource<Attendance>(myKids);
+    });
+
+    readonly myKidsTotal = computed(() => this.dataSource().data.length);
+    readonly myKidsOnBoard = computed(() => this.dataSource().data.filter(a => a.status === 'On board').length);
+    readonly myKidsArrived = computed(() => this.dataSource().data.filter(a => a.status === 'Boarded').length);
+
+    ngOnInit() {
+        this.http.get<any>(`${environment.apiBaseUrl}/currentUser`).subscribe({
+            next: (user) => {
+                if (user) {
+                    this.currentParentName.set(`${user.firstName} ${user.lastName}`);
+                }
+            }
+        });
+    }
+}
