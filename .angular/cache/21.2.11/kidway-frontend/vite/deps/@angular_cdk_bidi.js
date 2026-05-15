f38b23835fedf7002f656bf90e23f11273932998
tomitomi10 <168614425+tomitomi10@users.noreply.github.com>
2026-05-14 20:15:10 -0500

    feat(tracking): add district field to location model

---
+++ .angular/cache/21.2.11/kidway-frontend/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,15 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-IETCOAMT.js";
+import "./chunk-A5K7R6VL.js";
+import "./chunk-RSS3ODKE.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
--- src/tracking/domain/models/location.model.ts
+++ src/tracking/domain/models/location.model.ts
@@ -2,6 +2,7 @@ export interface VehicleLocation {
   vehicleId: string;
   latitude: number;
   longitude: number;
+  district: string;
   speed: number;
   updatedAt: Date;
 }
