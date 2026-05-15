5766cd44155a908d1a15b930b84c2846dca582bf
tomitomi10 <168614425+tomitomi10@users.noreply.github.com>
2026-05-14 20:15:44 -0500

    feat(tracking): simulate multiple vehicles in Lima districts

--- src/tracking/infrastructure/http/tracking-socket.service.ts
+++ src/tracking/infrastructure/http/tracking-socket.service.ts
@@ -4,15 +4,22 @@ import { VehicleLocation } from '../../domain/models/location.model';
 
 @Injectable({ providedIn: 'root' })
 export class TrackingSocketService {
-  public getLiveUpdates(): Observable<VehicleLocation> {
+  private vehicles = [
+    { id: 'BUS-101', dist: 'Surco', lat: -12.129, lng: -77.001 },
+    { id: 'BUS-202', dist: 'San Borja', lat: -12.107, lng: -76.999 },
+    { id: 'BUS-303', dist: 'La Molina', lat: -12.071, lng: -76.942 }
+  ];
+
+  public getLiveUpdates(): Observable<VehicleLocation[]> { // Ahora devuelve un array
     return interval(3000).pipe(
-      map(() => ({
-        vehicleId: 'BUS-001',
-        latitude: -12.086 + (Math.random() * 0.001), // Simulación en Lima
-        longitude: -77.031 + (Math.random() * 0.001),
-        speed: 35 + Math.random() * 10,
+      map(() => this.vehicles.map(v => ({
+        vehicleId: v.id,
+        district: v.dist,
+        latitude: v.lat + (Math.random() * 0.002),
+        longitude: v.lng + (Math.random() * 0.002),
+        speed: 20 + Math.random() * 15,
         updatedAt: new Date()
-      }))
+      })))
     );
   }
 }
